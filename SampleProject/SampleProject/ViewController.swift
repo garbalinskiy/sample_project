00d52ec43f2642715e099371d11b2b632f6fd5a9
Sergey Garbalinskiy <sergey.garbalinskiy@gmail.com>
2023-03-13 12:27:01 +0100

    added task description

--- SampleProject/SampleProject/ViewController.swift
+++ SampleProject/SampleProject/ViewController.swift
@@ -1,10 +1,12 @@
 import UIKit
 
 ///
-/// 1. Create a module VIPER style that will get User from ApiService
-/// 2. Module should not use callbacks and must know nothing about API
-///
-///
+/// 1. Create a module in a VIPER style that will get User from ApiService
+/// 2. Module should not use callbacks and must know nothing about API layer
+/// 3. RxSwift already imported using SPM
+/// 4. Add a button on that module which becomes enabled when User name is displayed
+/// 5. Clicking on that button should trigger Module's delegate method
+/// 6. Implement somewhere delegate of the module and output a message
 ///
 
 class ViewController: UIViewController {
